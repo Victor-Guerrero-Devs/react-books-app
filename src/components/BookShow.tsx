2f53ffa7bd86186781210cb06916bc47d6ceaa44
Victor Guerrero <vicguerrero.dev@gmail.com>
2023-07-01 16:22:25 -0500

    Refactor book delete functionality

--- src/components/BookShow.tsx
+++ src/components/BookShow.tsx
@@ -6,9 +6,17 @@ type BookShowProps = {
 };
 
 const BookShow = ({ book, onDeleteBook }: BookShowProps) => {
+  const handleClick = () => {
+    onDeleteBook(book.id);
+  };
   return (
-    <div className="book-show" onClick={() => onDeleteBook(book.id)}>
+    <div className="book-show">
       {book.title}
+      <div className="actions">
+        <button className="delete" onClick={handleClick}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
