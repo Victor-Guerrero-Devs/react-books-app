8326d3cd12453ac809e4f52ae670571c7e5144c8
Victor Guerrero <vicguerrero.dev@gmail.com>
2023-07-01 15:09:08 -0500

    Build add book functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 const App = () => {
+  const [books, setBooks] = useState<string[]>([]);
+
+  const createBookHandler = (newBook: string) => {
+    setBooks([...books, newBook]);
+    console.log(books);
+  };
   return (
     <>
       <h1>Hello world</h1>
+      <BookCreate onBookCreate={createBookHandler} />
     </>
   );
 };
--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState } from "react";
 
-const BookCreate = () => {
-  return <div>BookCreate</div>;
+type BookCreateProps = {
+  onBookCreate: (newBook: string) => void;
+};
+
+const BookCreate = ({ onBookCreate }: BookCreateProps) => {
+  const [title, setTitle] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onBookCreate(title);
+    setTitle("");
+  };
+
+  const handleTitleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            onChange={handleTitleChange}
+            value={title || ""}
+            type="text"
+            id="title"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default BookCreate;
